283b2e5a66db3456b7e381314bad7b477bb40b99
Snehasish1099 <100360925+Snehasish1099@users.noreply.github.com>
2022-07-13 15:57:44 +0530

    Add files via upload

---
+++ library/admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2021 Online Library Management System | Designed by : Sudip Barik (MCA 2004542)</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
